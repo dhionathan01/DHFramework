4bb698d420a4be3932cf68fe4b9a50edfba1fc93
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2023-02-12 01:46:54 -0300

    Criando rotas  e IndexController funcional

---
+++ App/Controllers/IndexController.php
@@ -0,0 +1,12 @@
+<?php 
+    namespace App\Controllers;
+
+    class indexController{
+        public function index() {
+            echo "Chegamos ao IndexController e dispararemos  a action index";
+        }
+        public function sobreNos() {
+            echo "Chegamos ao IndexController e dispararemos  a action sobreNos";
+        }
+    }
+?>
--- public/index.php
+++ public/index.php
@@ -1,4 +1,10 @@
 ﻿<? 
-    require_once"../vendor/";
-    echo 'Isso está funcionando';
+    function exibirArray($array){
+        echo "<pre>";
+        print_r($array);
+        echo "</pre>";
+    }
+    
+    require_once"../vendor/autoload.php";
+    $route = new \App\Route;
 ?>
