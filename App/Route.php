04609f713f712670b09724f4b5cbf3fdfafd1b04
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2023-02-12 21:22:18 -0300

    Abstração classe Route, Separando requisitos funcionais de não funcionais

--- App/Route.php
+++ App/Route.php
@@ -1,21 +1,10 @@
 <? 
     namespace App;
-    class Route{
-        private $routes;
 
-        public function __construct(){
-            $this->initRoutes();
-            $this->run($this->getUrl());
-        }
+    use DHF\Init\Bootstrap; 
+    class Route extends Bootstrap{
 
-        public function getRoutes(){
-            return $this->routes;
-        }
-        public function setRoutes(array $routes){
-            $this->routes = $routes;
-        }
-
-        public function initRoutes(){
+        protected function initRoutes(){
             $routes['home'] = array(
                 'route' => '/',
                 'controller' => 'indexController',
@@ -27,28 +16,7 @@
                 'controller' => 'indexController',
                 'action' => 'sobreNos'
             );
-
             $this->setRoutes($routes);
-        }
-
-        public function run($url){
-            foreach($this->getRoutes() as $path => $route){
-                if($url == $route['route']){
-                    $class = "App\\Controllers\\".ucfirst($route['controller']);
-                    
-                    // instanciando uma classe de forma dinâmica
-                    $controller = new $class;
-                    // chamando uma função de forma dinâmina na classe dinâmica
-                    $action = $route['action'];
-                    // usando o valor contido na variavel action para chamar uma função
-                    $controller->$action() ;
-
-                }
-            }
-        }
-
-        function getUrl(){
-            return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
         }
     }
 ?>
